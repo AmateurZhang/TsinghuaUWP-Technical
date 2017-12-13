3374a1f1b0c3e87e8d17204e2577661e47b21466
tantao14 <352640644@qq.com>
2017-12-13 18:45:55 +0800

    2rd-iteration
    2rd-iteration

---
+++ TsinghuaUWP-Technical-dev-zhang/ClassRoomAPI/ClassRoomAPI/Models/ClassRoomInfoModels.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassRoomAPI.Models
+{
+    //Consts
+    public enum ParseDataMode
+    {
+        Remote,
+        Local,
+        Demo
+    }
+
+    public class ExceptionCodeClassRoomInfo
+    {
+
+        //Define Exception Codes
+        public static int EXCEPTION_RETURN_LOCAL_DATA_FAILED = 1001;
+        public static int EXCEPTION_RETURN_REMOTE_DATA_FAILED = 1002;
+    }
+
+    //DataStruct
+    
+    public class BuildingTypeNamesData //记录所有教学楼名字的数据结构
+    {
+        public string PositionName;
+        public string DetailUri;
+    }
+    public class ClassRoomStatueData  //记录教学楼中每个教室的数据结构
+    {
+        
+        public string ClassRoomName;
+
+        //6节课1-6
+        public List<string> ListClassStatus;
+        //true=占用
+        public List<bool> ListBoolClassStatus;
+
+        public List<string> ConvertBooltoSymbol
+        {
+            get
+            {
+                var _Return=new List<string>();
+                for(int i=0;i<ListBoolClassStatus.Count;i++)
+                    try
+                    {
+                         if (ListBoolClassStatus[i] == true)
+                         {
+                            _Return.Add("\xEDAF");
+                         }
+                         else
+                         {
+                        _Return.Add("\xECCA");
+
+                          }
+                     }
+                     catch
+                    {
+                        _Return.Add("\xECCA");
+                     }
+                return _Return;
+            }
+            set {; }
+            
+        }
+    }
+
+    public class BuildingInfoData
+    {
+        public string BuildingName;
+        public List<ClassRoomStatueData> ListBuildingInfoData;
+    }
+
+    public class ClassBuildingInfo
+    {
+        public DateTime Date;
+        public List<BuildingTypeNamesData> ListClassRoomInfo;
+        public List<BuildingInfoData> ListClassRoomStatue;
+    }
+
+    
+}
---
+++ TsinghuaUWP-Technical-dev-zhang/ClassRoomAPI/ClassRoomAPI/Views/Performance.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ClassRoomAPI.Models;
+using ClassRoomAPI.ViewModels;
+using ClassRoomAPI.Controls;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace ClassRoomAPI.Views
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Performance : Page
+    {
+        public Performance()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var _Data = await PerformaceViewModels.GetHallInfoViewModel(ParseDataMode.Local);
+                if ((_Data.Date.Date - DateTime.Now.Date).Days < 0)
+                    throw new Exception("The Data are out-of-date.");
+                else
+                    MainPivot.ItemsSource = _Data.ListShowInfo;
+                var notifyPopup = new NotifyPopup("正在使用本地数据。");
+                notifyPopup.Show();
+            }
+            catch
+            {
+                try
+                {
+                    var _DataRemote = await PerformaceViewModels.GetHallInfoViewModel(ParseDataMode.Remote);
+                    MainPivot.ItemsSource = _DataRemote.ListShowInfo;
+                    var notifyPopup = new NotifyPopup("演出信息已更新！");
+                    notifyPopup.Show();
+                }
+                catch
+                {
+                    var notifyPopup = new NotifyPopup("网络异常，刷新失败。");
+                    notifyPopup.Show();
+                }
+
+            }
+        }
+    }
+
+}
